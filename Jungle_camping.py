93289b37a67f43c60a2c82bdddfb1908f52af945
Etim Nse-Abasi <etimnseabasi@gmail.com>
2020-07-14 12:32:47 +0100

    Create Jungle_camping.py

---
+++ Jungle_camping.py
@@ -0,0 +1,34 @@
+'''
+Jungle Camping
+
+You are camping alone out in the jungle and you hear some animals in the dark nearby. Based on the noise they make, determine which animals they are.
+
+Task: 
+You are given the noises made by different animals that you can hear in the dark, evaluate each noise to determine which animal it belongs to. Lions say 'Grr', Tigers say 'Rawr', Snakes say 'Ssss', and Birds say 'Chirp'.
+
+Input Format: 
+A string that represent the noises that you hear with a space between them.
+
+Output Format: 
+A string that includes each animal that you hear with a space after each one. (animals can repeat)
+
+Sample Input: 
+Rawr Chirp Ssss
+
+Sample Output: 
+Tiger Bird Snake
+'''
+
+
+sound=str(input()).split()
+soln=[]
+for i in sound:
+    if i=="Grr":
+       soln.append("Lion")
+    elif i=="Rawr":
+        soln.append("Tiger")
+    elif i=="Ssss":
+        soln.append("Snake")
+    elif i=="Chirp":
+        soln.append("Bird")
+print(" ".join(soln))
